cd029ef20f32c84b93547d1b0ab521f6d2aa3448
Miguel E. Ruiz <meruiz77@gmail.com>
2020-07-26 20:54:00 +0200

    Add index users action. Cleanup code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
-  def robohash_for(user, options = { set: 1 })
+  def robohash_for(user, options = { size: 200 })
     username = user.username.downcase
-    set = options[:set]
-    robohash_url = "https://robohash.org/#{username}?set=set#{set}"
+    size = options[:size]
+    robohash_url = "https://robohash.org/#{username}?size=#{size}x#{size}"
     image_tag(robohash_url, alt: user.username, class: "rounded mx-auto shadow bg-light d-block")
   end
 end
