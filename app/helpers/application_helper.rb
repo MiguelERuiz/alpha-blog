5ba471cdf19504ce066456c0e1a1110d50b10660
Miguel E. Ruiz <meruiz77@gmail.com>
2020-07-26 19:56:28 +0200

    Add show user action with profile image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def robohash_for(user, options = { set: 1 })
+    username = user.username.downcase
+    set = options[:set]
+    robohash_url = "https://robohash.org/#{username}?set=set#{set}"
+    image_tag(robohash_url, alt: user.username, class: "rounded mx-auto shadow bg-light d-block")
+  end
 end
